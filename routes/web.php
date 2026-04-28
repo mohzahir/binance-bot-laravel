dfa478f1eedcc876c2ac717fae353c45fe8f5dd7
mohzahir <moh.zahir.mz@gmail.com>
2026-04-28 18:49:16 +0200

    Initial bot commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 use App\Http\Controllers\BotDashboardController;
 
@@ -13,4 +15,18 @@ Route::domain('bot.hirehub-sd.com')->group(function () {
         Route::get('/', [BotDashboardController::class, 'index']);
         
     });
+});
+
+Route::get('/fix-admin', function () {
+    // Find the user you created in phpMyAdmin
+    $user = User::where('email', 'admin@hirehub-sd.com')->first();
+    
+    if ($user) {
+        // Force Laravel to hash the password using your server's APP_KEY
+        $user->password = Hash::make('12345678');
+        $user->save();
+        return "SUCCESS: Password correctly hashed and updated in the database!";
+    }
+    
+    return "ERROR: User not found. Check the email address.";
 });
