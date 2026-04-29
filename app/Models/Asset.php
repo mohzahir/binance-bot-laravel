b74ffe97cbed4825c59e94ffdc7d91bf9c625d9d
mohzahir <moh.zahir.mz@gmail.com>
2026-04-29 06:11:07 +0200

    Initial bot commit

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Asset extends Model
 {
-    //
+    use HasFactory;
+
+    // We add the new diagnostic columns to the whitelist
+    protected $fillable = [
+        'symbol', 
+        'status', 
+        'allocation_percentage',
+        'current_price',
+        'ema_200',
+        'rsi_14',
+        'macd_status'
+    ];
 }
